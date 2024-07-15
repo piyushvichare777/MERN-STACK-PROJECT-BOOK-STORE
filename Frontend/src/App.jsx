31d244d895dc149e990eba1d39f3908e7e0b9f73
Piyushvichare77@gmail.com <piyushvichare777>
2024-07-15 10:31:40 +0530

    Navbar is done

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Navbar from './Component/Navbar'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Navbar/>
+    
+    </>
+  )
+}
+
+export default App
